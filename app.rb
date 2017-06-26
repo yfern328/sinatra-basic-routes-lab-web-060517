ec07e5ae57547e2f46378f3dbe36815790bdbd1e
yfern328 <yfernan2@u.rochester.edu>
2017-06-26 15:57:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do "Hello, World!"
+  end
+
+  get "/name" do "My name is Yelstin"
+  end
+
+  get "/hometown" do "My hometown is New York"
+  end
+
+  get "/favorite-song" do "My favorite song is Hello"
+  end
+
 end
